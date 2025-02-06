5b02214aee4dd3458c2b61881ea7ceb6ffcbe43f
cyilmaze <cyilmaze@cisco.com>
2025-02-06 16:45:06 +0100

    script added

---
+++ scripts/prepare_host.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prepare host for ThousandEyes BrowserBot Container
+curl -Os https://downloads.thousandeyes.com/bbot/configure_docker.sh
+chmod +x configure_docker.sh
+sudo ./configure_docker.sh
+
+# Kubernetes requires seccomp profiles files under directory /var/lib/kubelet/seccomp. Crete the seccomp directory.
+sudo mkdir /var/lib/kubelet/seccomp
+sudo cp /var/docker/configs/te-seccomp.json /var/lib/kubelet/seccomp/
